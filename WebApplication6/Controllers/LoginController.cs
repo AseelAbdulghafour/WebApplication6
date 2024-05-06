d2e5af1d85c39597bf1e43837f69e6be32eadf79
83315 <alshaibaninada@gmail.com>
2024-05-06 09:07:27 +0300

    Adding Register

--- WebApplication6/Controllers/LoginController.cs
+++ WebApplication6/Controllers/LoginController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Models.Entites;
+using SQLitePCL;
+using WebApplication6.Model;
+using WebApplication6.Model.Request;
 using static WebApplication6.Services.AuthService;
 
 namespace WebApplication6.Controllers
@@ -8,16 +13,19 @@ namespace WebApplication6.Controllers
         [ApiController]
         public class LoginController : ControllerBase
         {
-            private readonly TokenService service;
+            private readonly TokenService _service;
+            private readonly DesignDistrictContext _context;
+        
 
-            public LoginController(TokenService service)
+        public LoginController(TokenService service, DesignDistrictContext context)
             {
-                this.service = service;
+                _service = service;
+                _context = context;
             }
-            [HttpPost]
+            [HttpPost("Login")]
             public IActionResult Login(string username, string password)
             {
-                var response = this.service.GenerateToken(username, password);
+                var response = _service.GenerateToken(username, password);
 
                 if (response.IsValid)
                 {
@@ -34,6 +42,26 @@ namespace WebApplication6.Controllers
         {
             public string Username { get; set; }
             public string Password { get; set; }
+        }
+
+
+        [HttpPost("Registor")]
+        public IActionResult Registor(SignupRequest request)
+        {
+            
+            var newUser = UserAccount.Create(request.Username, request.Password, request.Email, request.PhoneNumber, request.Address, request.IsAdmin);
+            newUser.Username = request.Username;
+            newUser.Email = request.Email;
+            newUser.Address = request.Address;
+            newUser.PhoneNumber = request.PhoneNumber;
+
+            _context.UserAccounts.Add(newUser);
+            _context.SaveChanges();
+
+            return Created(nameof(UserAccount), newUser);
+
+            return Ok(new { Message = "User Created" });
+
         }
 
     }
--- WebApplication6/Model/Request/SignupRequest.cs
+++ WebApplication6/Model/Request/SignupRequest.cs
@@ -5,7 +5,8 @@
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public string CivilId { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Address { get; set; }
         public bool IsAdmin { get; set; }
     }
 }
